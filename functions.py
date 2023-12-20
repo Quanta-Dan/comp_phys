c2ffa2338f3f718bfa9031fac2b8e1f39808d670
Daniel Cierpinsky <daniel.cierpinsky@gmail.com>
2023-12-20 17:18:03 +0100

    finished time evolution

--- functions.py
+++ functions.py
@@ -127,7 +127,7 @@ def potential_function(psi: np.ndarray)->np.ndarray:
     """
     Applies the potential to the wavefunction.
     This function requires the global variable potential_array to be defined, as it is used for the calculation.
-    The operation consists of an array multiplication between the potential array and the wavefunction.
+    The function consists of an array multiplication between the potential array and the wavefunction.
     ## Parameters
     - psi: numpy.ndarray, the wavefunction of size NxD onto which the potential is to be applied.
 
@@ -146,7 +146,14 @@ def potential_function(psi: np.ndarray)->np.ndarray:
 
 def kinetic_energy_function(psi: np.ndarray)->np.ndarray:
     """Kinetic energy function, which takes ψ and returns Hψ for the particular case V = 0.
-    mu, epsilon, N, D are requriered global variables"""
+    mu, epsilon, N, D are requiered global variables.
+    The function calculates the discretized gradient using the wavefunction value of the nearest neighbour lattice points.
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD onto which the potential is to be applied.
+
+    ## Returns
+    - numpy.ndarray: kinetic energy operator applied to the array of wavefunction.
+    """
 
     assert isinstance(psi,np.ndarray) , "psi must be a ndarray"
 
@@ -161,7 +168,14 @@ def kinetic_energy_function(psi: np.ndarray)->np.ndarray:
 
 
 def hamiltonian_function(psi: np.ndarray)->np.ndarray:
-    """Hamiltonian function, which takes ψ and returns Hψ. This function uses kinetic_energy_function  and potential_function."""
+    """Hamiltonian function, which takes ψ and returns Hψ.
+    This function calls the  kinetic_energy_function  and potential_function and adds their outputs.
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD onto which the potential is to be applied.
+
+    ## Returns
+    - numpy.ndarray: hamiltonian operator applied to the array of wavefunction.
+    """
 
     assert isinstance(psi,np.ndarray) , "psi must be a ndarray"
 
@@ -239,7 +253,22 @@ def conjugate_gradient(apply_A,b,epsilon, max_iters = 10000):
 
 def lowest_eigenvalue_vector(hamiltonian:callable,  power_method_tolerance: float, conjugate_gradient_tolerance: float,
                                 max_iters_power_method = 10000, max_iters_conjugate_gradient= 10000):
-    """Function that calculates the smallest eigenvalue anhd corresponding eigenvector of a matrix"""
+    """
+    Function that calculates the smallest eigenvalue and corresponding eigenvector of the hamiltonian for the given lattice.
+    This function calls the  power_method function and provides it the conjugate_gradient of the hamiltonian function applied on the wavefunction.
+    Effectively the power method is applied on the inverse hamiltonian operator, resulting in the smalles eigenvalue and vector for the hamiltonian..
+    mu, epsilon, N, D are requiered global variables.
+    ## Parameters
+    - hamiltonian: callable, the hamiltonian function of which the eigenvalue and eigenfunction will be calculated.
+    - power_method_tolerance: float, required precision of the power method function.
+    - conjugate_method_tolerance: float, required of the conjugate gradient function.
+    - max_iters_power_method: int (optional), maximum iterations of the power method function
+    - max_iters_conjugate_gradient: int (optional), maximum iterations of the conjugate gradient function
+
+    ## Returns
+    - mu: float, smallest eigenvalue of hamiltonian operator.
+    - v: numpy.ndarray, eigenfunction corresponding to mu.
+    """
     global A, D, N
 
    
@@ -257,11 +286,27 @@ def lowest_eigenvalue_vector(hamiltonian:callable,  power_method_tolerance: floa
 
 
 def expectation_energy(psi:np.ndarray)->float:
-    """Calculates expectation value of energy for wavefunction psi."""
+    """
+    Calculates expectation value of energy for wavefunction psi.
+    Function calculates the array product of psi, H(psi).
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD.
+
+    ## Returns
+    - float: expectation value of energy.
+    """
     return np.vdot(psi, hamiltonian_function(psi)).real
 
 def expectation_position(psi:np.ndarray)->float:
-    """Calculates expectation value of position for wavefunction psi."""
+    """
+    Calculates expectation value of position for wavefunction psi.
+    Function calculates the array product of psi, position*psi.
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD.
+
+    ## Returns
+    - numpy.ndarray: expectation value of position as an array.
+    """
     global D
     position_vector = np.empty((D,1))
     for i in range(0,D):
@@ -269,7 +314,15 @@ def expectation_position(psi:np.ndarray)->float:
     return position_vector
 
 def expectation_momentum(psi:np.ndarray)->float:
-    """Calculates expectation value of momentum for wavefunction psi."""
+    """
+    Calculates expectation value of momentum for wavefunction psi.
+    Function calculates the array product of psi, derivative(psi).
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD.
+
+    ## Returns
+    - numpy.ndarray: expectation value of momentum as an array.
+    """
     global mu, epsilon, N, D
     momentum_expct = np.zeros((D,1))
     for i in range(D):
@@ -278,7 +331,15 @@ def expectation_momentum(psi:np.ndarray)->float:
     return momentum_expct
 
 def indetermination_position(psi:np.ndarray)->float:
-    """Calculates indetermination of position for wavefunction psi."""
+    """
+    Calculates indetermination of position for wavefunction psi.
+    Function calculates the array product of psi, position^2*psi.
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD.
+
+    ## Returns
+    - numpy.ndarray: indetermination of position as an array.
+    """
     global D
     position2_vector = np.empty((D,1))
     for i in range(0,D):
@@ -286,7 +347,15 @@ def indetermination_position(psi:np.ndarray)->float:
     return position2_vector-expectation_position(psi)**2
 
 def indetermination_momentum(psi:np.ndarray)->float:
-    """Calculates indetermination of momentum for wavefunction psi."""
+    """
+    Calculates indetermination of momentum for wavefunction psi.
+    Function calculates the array product of psi, derivative^2(psi).
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD.
+
+    ## Returns
+    - numpy.ndarray: indetermination of momentum as an array.
+    """
     global mu, epsilon_2, N, D
     second_derivative = np.zeros((D,1))
     for i in range(D):
@@ -295,21 +364,54 @@ def indetermination_momentum(psi:np.ndarray)->float:
     return second_derivative-expectation_momentum(psi)**2
 
 def probability_xg0(psi:np.ndarray)->float:
-    """Calculates probability that the particle is found in x>0 for wavefunction psi."""
+    """
+    Calculates probability that the particle is found in x>0 for wavefunction psi.
+    Function calculates the norm of the wavefunction for x>0 divided by the full norm.
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD.
+
+    ## Returns
+    - float: probability that the particle is found in x>0.
+    """
     psi_l0, psi_g0 = np.array_split(psi, 2, axis = 0)
     return np.vdot(psi_g0, psi_g0).real/np.vdot(psi, psi).real
 
 def norm(psi:np.ndarray)->float:
-    """Calculates norm of wavefunction psi."""
+    """
+    Calculates norm of wavefunction psi.
+    Function calculates the norm of the wavefunction as array product of psi,psi.
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD.
+
+    ## Returns
+    - float: norm of the wavefunciton psi.
+    """
     norm = np.vdot(psi,psi).real
     return norm
     
 def expectation_kinetic_energy(psi:np.ndarray)->float:
-    """Calculates expectation value of Kinetic energy for wavefunction psi."""
+    """
+    Calculates expectation value of Kinetic energy for wavefunction psi.
+    Function calculates the array product of psi, Ekin(psi).
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD.
+
+    ## Returns
+    - float: expectation value of kinetic energy.
+    """
+    
     return np.vdot(psi, kinetic_energy_function(psi)).real
 
 def expectation_potential_energy(psi:np.ndarray)->float:
-    """Calculates expectation value of Potential energy for wavefunction psi."""
+    """
+    Calculates expectation value of Potential energy for wavefunction psi.
+    Function calculates the array product of psi, Epot(psi).
+    ## Parameters
+    - psi: numpy.ndarray, the wavefunction of size NxD.
+
+    ## Returns
+    - float: expectation value of potential energy.
+    """
     return np.vdot(psi, potential_function(psi)).real
 
 ############ Time Evolution ##############
@@ -317,12 +419,12 @@ def expectation_potential_energy(psi:np.ndarray)->float:
 def generate_gauss(k0: np.ndarray, origin:np.ndarray, width: float)->np.ndarray:
     """
     Generate a square D-dimensional array of size N representing a gaussian wave packet.
-
-    Parameters:
+    ## Parameters:
     - k0: np.ndarray, maximum of gaussian in momentum space.
     - origin: tuple, origin coordinates (relative to center) of the gaussian wave packet
-    Returns:
-    - np.ndarray for the wavefunction 
+    - width: float, width of the gaussian curve
+    ## Returns:
+    - np.ndarray: array for the wavefunction 
     """
     global N, D
     ix = np.ndindex((N,)*D)
@@ -340,11 +442,31 @@ def generate_gauss(k0: np.ndarray, origin:np.ndarray, width: float)->np.ndarray:
 
 
 def integrator_euler(psi:np.ndarray, time_step: float, hamiltonian:callable)->np.ndarray:
-    """Evolves function psi by provided timestep."""
+    """
+    Evolves function psi by provided timestep.
+    The function calculates : psi-i*time_step*Hamiltonian(psi).
+    ## Parameters:
+    - psi: np.ndarray, wavefunction to be evolved.
+    - time_step: float, time step of the evolution 
+    - hamiltonian: callable, hamiltonian function to be used during evolution
+    ## Returns:
+    - np.ndarray: Array of the wavefunction evolved by time_step
+    """
     psi_evolved = psi - 1j*time_step*hamiltonian(psi)
     return psi_evolved
 
 def integrator_crank_nicolson(psi:np.ndarray, time_step: float, hamiltonian:callable, conjugate_gradient_tolerance= 1.e-10, max_iters_conjugate_gradient= 100000):
+    """
+    Evolves function psi by provided timestep.
+    The function calls the conjugate gradient function to calculate the inverse of an operator array.
+    ## Parameters:
+    - psi: np.ndarray, wavefunction to be evolved.
+    - time_step: float, time step of the evolution 
+    - hamiltonian: callable, hamiltonian function to be used during evolution
+    ## Returns:
+    - np.ndarray: Array of the wavefunction evolved by time_step
+    """
+    
     def inversion_term(psi):
         """Term to be inverted by conjugate gradient"""
         return psi+0.25*time_step**2*hamiltonian(hamiltonian(psi))
@@ -354,21 +476,41 @@ def integrator_crank_nicolson(psi:np.ndarray, time_step: float, hamiltonian:call
     return psi_evolved
 
 def kinetic_energy_array_calculator()->np.ndarray:
-        global N, D, mu, epsilon_2
-        ix = np.ndindex((N,)*D)
-        kinetic_energy = np.zeros((N,)*D,dtype=complex)
-        for index in ix:
-            shifted_index = np.array(index)-np.floor(N/2)
-            shifted_index_flat = shifted_index.ravel()
-            shifted_index_k_flat = np.fft.fft(shifted_index_flat)
-            shifted_index_k = shifted_index_k_flat.reshape(shifted_index.shape)
-            sum = 0
-            for i in range(D):
-                sum += ((np.sin(np.pi*shifted_index_k[i]/N)))**2
-            kinetic_energy[index] = (2/(mu*epsilon_2))*sum
-        return kinetic_energy
+    """
+    Calculates kinetic energy array by means of hamiltonian eigenvalues for given parameters.
+    The function output is used in the integrator_strang_splitting function.
+    The function requires N, D, mu, epsilon_2 as global variables.
+    ## Parameters:
+    - none
+    ## Returns:
+    - np.ndarray: kinetic energy array to be used in integrator_strang_splitting function
+    """
+    global N, D, mu, epsilon_2
+    ix = np.ndindex((N,)*D)
+    kinetic_energy = np.zeros((N,)*D,dtype=complex)
+    for index in ix:
+        shifted_index = np.array(index)-np.floor(N/2)
+        shifted_index_flat = shifted_index.ravel()
+        shifted_index_k_flat = np.fft.fft(shifted_index_flat)
+        shifted_index_k = shifted_index_k_flat.reshape(shifted_index.shape)
+        sum = 0
+        for i in range(D):
+            sum += ((np.sin(np.pi*shifted_index_k[i]/N)))**2
+        kinetic_energy[index] = (2/(mu*epsilon_2))*sum
+    return kinetic_energy
 
 def integrator_strang_splitting(psi:np.ndarray, time_step: float, hamiltonian):
+    """
+    Evolves function psi by provided timestep.
+    The function uses the kinetic_energy_array_calculator function as well as fast fourier transformation library provided by numpy.
+    The function requires N, D, mu, epsilon, potential_array, kinetic_energy_array as global variables.
+    ## Parameters:
+    - psi: np.ndarray, wavefunction to be evolved.
+    - time_step: float, time step of the evolution 
+    - hamiltonian: callable (not used, compatibility), hamiltonian function to be used during evolution
+    ## Returns:
+    - np.ndarray: Array of the wavefunction evolved by time_step
+    """
     global N, D, mu, epsilon, potential_array, kinetic_energy_array    
 
     eta_real = np.exp(-0.5j*time_step*potential_array)*psi
@@ -385,9 +527,22 @@ def integrator_strang_splitting(psi:np.ndarray, time_step: float, hamiltonian):
 
 
 
-# main call 
+
 
 def time_evolution(psi:np.ndarray, hamiltonian, integrator:callable, time_step: float, time_total: float)->pd.DataFrame:
+    """
+    Function that iteratively calculates the time evolution using the selected integrator.
+    The function iterates until the target time is used, while storing the wavefunction array and the observables as a dataframe.
+
+    ## Parameters:
+    - psi: np.ndarray, wavefunction to be evolved.
+    - time_step: float, time step of the evolution. 
+    - time_total: flaot, total time to be reached through iteration.
+    - integrator: callable, integrator to be used for the time evolution.
+    - hamiltonian: callable, hamiltonian function to be used during evolution.
+    ## Returns:
+    - pandas.dataframe: dataframe containing the avefunction and all observables for each time_step
+    """
     assert callable(integrator) , "ntegrator must be a function"
     assert isinstance(time_step, float) and time_step > 0. , "time_step=" + str(time_step) + " must be a positive float"
     assert isinstance(time_total, float) and time_total > 0. , "time_total=" + str(time_total) + " must be a positive float"
@@ -407,7 +562,21 @@ def time_evolution(psi:np.ndarray, hamiltonian, integrator:callable, time_step: 
     return evolution_log
 
 
-def plot_animation(df, time_resolution, hamiltonian, file_name):
+def plot_animation(df, time_resolution, hamiltonian, file_name, title):
+    """
+    Function plots the wavefunction animation over time.
+    The plotted function is the absolute square of the function as well as the potential.
+
+    ## Parameters:
+    - df: pandas.dataframe, contains all data on time evolved function.
+    - time_resolution: float, factor of how many of all steps should be plotted. 
+    - hamiltonian: callable, hamiltonian function that was used during evolution.
+    - file_name: str, filename of the animation to be saved.
+    - title: str, title to be displayed on the animation
+    ## Returns:
+    - pandas.dataframe: dataframe containing the avefunction and all observables for each time_step
+    """
+
     global N, D, epsilon
 
     df_selected = df.iloc[::time_resolution]
@@ -415,8 +584,8 @@ def plot_animation(df, time_resolution, hamiltonian, file_name):
     x = np.zeros((N,)*D)
     x_axis = np.linspace(-N/2,N/2,N)
     x_axis= x_axis*epsilon
-    max_function_value = df_selected['function'].apply(lambda x: np.max(x).real / epsilon**(0.5)).max()
-    min_function_value = df_selected['function'].apply(lambda x: np.min(x).real / epsilon**(0.5)).min()
+    max_function_value = df_selected['function'].apply(lambda x: np.max(x**2) / epsilon).max()
+    min_function_value = df_selected['function'].apply(lambda x: np.min(x**2) / epsilon).min()
 
 
     fig, ax = plt.subplots()
@@ -429,17 +598,17 @@ def plot_animation(df, time_resolution, hamiltonian, file_name):
     def update(frame):
         
         ax.clear()  # Clear the previous frame
-        ax.set_ylabel(r"$\Psi/ \varepsilon^{\frac{1}{2}}$")
+        ax.set_ylabel(r"$|\Psi|^{2}/ \varepsilon$")
         ax.set_xlabel(r"$\frac{x}{r}$")
-        ax.set_title('Time evolution of plane wave in 1D')
+        ax.set_title(title)
         time = df_selected['time'].iloc[frame]
         function = df_selected['function'].iloc[frame]
         potential = potential_function(np.ones_like(x))
 
         # Plot the function and potential
-        ax.plot(x_axis, function.real/ epsilon**(0.5), label='Function', color='C0')
+        ax.plot(x_axis, function**2/ epsilon, label='Function', color='C0')
         if hamiltonian == hamiltonian_function:
-            ax.plot(x_axis, potential.real/10, label=fr'Potential $\mu$={mu}, a.u.', color='C1')
+            ax.plot(x_axis, potential.real, label=fr'Potential $\mu$={mu}, a.u.', color='C1')
         ax.set_ylim(min_function_value, max_function_value)
         ax.text(0.75,0.15, fr'$\tau$={time:.5}', bbox={'facecolor': 'white', 'pad': 1},transform=plt.gcf().transFigure)
         # Set plot labels and title
